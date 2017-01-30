ae3079b5707abd8ae7973de8580455ec27d2a2d1
Eduardo Russo <russoedu@gmail.com>
2017-01-30 09:52:36 -0200

    [master] Corrected required function

--- validations/required.js
+++ validations/required.js
@@ -1,6 +1,6 @@
 module.exports = function(required, theData) {
   var response = true;
-  if (theData === undefined) {
+  if (typeof theData === 'undefined' || theData === '' || theData === null) {
     response = 'field_is_required';
   }
   return response;
